c857abdacbaf6bb43b4e35fedbd96481397a3eeb
Tobias Gjerstrup <tobiasgjerstrup@gmail.com>
2026-04-04 11:10:04 +0200

    init

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    { ignores: ['eslint.config.ts', 'dist/**', 'playground/**', '**/*.js'] },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            globals: globals.node,
+            parserOptions: {
+                project: ['./tsconfig.eslint.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    js.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@typescript-eslint/no-duplicate-type-constituents': 'error',
+        },
+    },
+]);
---
+++ test/main.test.js
@@ -0,0 +1,7 @@
+import { afterAll, beforeAll} from 'vitest';
+
+beforeAll(async () => {
+});
+
+afterAll(async () => {
+});
